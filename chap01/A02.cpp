25e4ec8748440c92a86dc79f1e0f23b8ac86cb8c
gett <kazuki.kobayashi0728@gmail.com>
2026-09-08 00:55:42 +0900

    chap01/02

---
+++ chap01/A02.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int N, X, A[100];
+bool contains = false;
+
+int main() {
+    cin >> N >> X;
+    for (int i = 0; i < N; i++) cin >> A[i];
+
+    for (int i = 0; i < N; i++) {
+        if (A[i] == X) {
+            contains = true;
+            break;
+        };
+    };
+
+    if (contains) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
---
+++ chap01/B02.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int A, B;
+bool contains = false;
+
+int main() {
+    cin >> A >> B;
+
+    for (int i = A; i <= B; i++) {
+        if (100 % i == 0) {
+            contains = true;
+            break;
+        }
+    }
+
+    if (contains) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
